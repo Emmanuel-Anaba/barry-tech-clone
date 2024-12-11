323a608090c9eafd985fc2205dd98528f4a24a55
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-12-11 01:05:05 +0100

    Added gradient text

--- components/cta-section.tsx
+++ components/cta-section.tsx
@@ -4,10 +4,10 @@ import { Button } from "./ui/button";
 
 export default function CtaSection() {
   return (
-    <section className="py-12">
-      <div className="my-container relative grid place-items-center gap-12 rounded-xl py-12">
-        <div className="conic-gradient" />
-        <h1 className="text-center">
+    <section className="py-12  relative">
+      <div className="conic-gradient" />
+      <div className="my-container grid place-items-center gap-12 rounded-xl py-12 glass">
+        <h1 className="text-center gradient-text">
           Ready To Transform Your Digital Experience?
         </h1>
         <Button
